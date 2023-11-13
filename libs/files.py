219735851c435c417961d60d5c51564ce8790662
richa-z <145004550+richa-z@users.noreply.github.com>
2023-11-13 10:18:48 +0100

    Add handlers to new commands.

--- libs/files.py
+++ libs/files.py
@@ -7,3 +7,19 @@ def delete(file):
         print(e)
         return False
     return True
+
+def delete_dir(dir):
+    try:
+        os.rmdir(dir)
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+def create_dir(dir):
+    try:
+        os.mkdir(dir)
+    except Exception as e:
+        print(e)
+        return False
+    return True
