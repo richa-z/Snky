15ce5e8c4a89bed713a38ae5b61b9f35d0926015
richa-z <145004550+richa-z@users.noreply.github.com>
2023-11-25 23:09:15 +0100

    Update files.py

--- libs/files.py
+++ libs/files.py
@@ -1,4 +1,5 @@
 import os
+from cryptography.fernet import Fernet
 
 def delete(file):
     try:
@@ -23,3 +24,43 @@ def create_dir(dir):
         print(e)
         return False
     return True
+
+def encrypt_file(file, key):
+    try:
+        with open(file, "rb") as f:
+            data = f.read()
+        with open(file, "wb") as f:
+            f.write(encrypt(data, key))
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+def decrypt_file(file, key):
+    try:
+        with open(file, "rb") as f:
+            data = f.read()
+        with open(file, "wb") as f:
+            f.write(decrypt(data, key))
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+def encrypt(data, key):
+    f = Fernet(key)
+    with open(bytes(data), "rb") as F:
+        file = f.read()
+        enc_data = f.encrypt(file)
+    
+    with open(bytes(data), "wb") as F:
+        F.write(enc_data)
+
+def decrypt(data, key):
+    f = Fernet(key)
+    with open(data, "rb") as F:
+        file = f.read()
+        dec_data = f.decrypt(file)
+    
+    with open(data, "wb") as F:
+        F.write(dec_data)
